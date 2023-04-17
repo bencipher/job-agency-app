71afd0a9c8c98a608d2de577387a9f1d214bf722
bcx <femolak@gmail.com>
2023-04-17 18:39:17 +0000

    -refactor docker-compose to automatically get env var from host environment without needing to pass external file, so local env can use the file based env, added additional env file for docker to .gitignore but the new env is not needed again as the var will be fed through host enviroment variables

--- agency_app/serializers.py
+++ agency_app/serializers.py
@@ -12,21 +12,3 @@ class JobSerializer(serializers.ModelSerializer):
         if value < 0:
             raise serializers.ValidationError('Rate must be non-negative')
         return value
-
-
-# # For validating requests
-# class GetJobQueryParamsSerializer(serializers.Serializer):
-#     perPage = serializers.IntegerField(min_value=1, max_value=100, default=10)
-#     page = serializers.IntegerField(min_value=1, default=1)
-#     sortBy = serializers.ChoiceField(choices=[
-#         ('datePosted', 'Date Posted'), ('rate', 'Rate')], default='datePosted')
-#     contractType = serializers.ChoiceField(
-#         choices=[('contract', 'Contract'), ('permanent', 'Permanent')], required=False)
-#     dateSincePosted = serializers.DateField(required=False)
-#
-#     def validate(self, data):
-#         if data['perPage'] < 1 or data['perPage'] > 100:
-#             raise serializers.ValidationError({'perPage': 'Must be between 1 and 100'})
-#         if data['page'] < 1:
-#             raise serializers.ValidationError({'page': 'Must be greater than or equal to 1'})
-#         return data
